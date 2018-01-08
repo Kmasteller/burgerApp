69e7936ccacecf4213bafd6275c37359d84fc235
Kmasteller <kennethmasteller@gmail.com>
2018-01-07 18:06:31 -0700

    linking

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require('method-override')
 
 var port = process.env.PORT || 3000;
 
@@ -9,6 +10,8 @@ var app = express();
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(methodOveride("_method"));
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
